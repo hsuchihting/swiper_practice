4a68d9edc8e9e3505cda2672bd56d4437ca6de9c
TimHsu <hsuchihting@gmail.com>
2020-06-16 23:17:34 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  speed: 2500,
+  autoplay: {
+    delay: 2000,
+  },
+  effect: 'cube',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+      }, 
+});
